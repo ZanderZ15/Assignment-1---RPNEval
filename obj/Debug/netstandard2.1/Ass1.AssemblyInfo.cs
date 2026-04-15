caa294913caa8d862dcc6851f227799544e9ba89
ZanderZ15 <zandermillerbrown@gmail.com>
2026-04-15 16:50:40 -0700

    built, now going to try to test

--- obj/Debug/netstandard2.1/Ass1.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/Ass1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04aad3a58a7d0c6a44e57f605bbc1bb825caf6bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe18c3e2f33cf4aca109d0d88c6ddfdb469fdd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ass1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
